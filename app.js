33fa4e0831d3dc5a38aad1faa9b138357f287fbe
George Huliaris <georgehuliaris@me.com>
2021-03-18 21:05:43 -0500

    save button clickable

--- app.js
+++ app.js
@@ -2,18 +2,18 @@
 // display formatted date and time at top of page
     //use jquery to find parent element to append date and time to 
     //append date and time
-    
-    
-// create dom elements for calendar hours
     //create classes in css for different background colors
     //loop through time-col and check agains current time 
+    // add click event to event column to activate text 
+    // create element to enter event 
+    
+    // save button with click event 
+    
+    // create dom elements for calendar hours
     //use jquery to append class based on past present future 
-        //change past hours to past class
+      //change past hours to past class
         //change current time to red
         //change future to future class / green color
-// add click event to event column to activate text 
-// create element to enter event 
-    // save button with click event 
     // saves event to local storage
 // get events from local storage so they dont disappear on page reload
 // load events into appropriate collumns on page refresh
@@ -31,4 +31,29 @@ $( "#currentDay" ).append(currentDateTime);
 const times = ['8 A.M.', '9 A.M.', '10 A.M.', '11 A.M.', '12 P.M.', '1 P.M.', '2 P.M.', '3 P.M.', '4 P.M.', '5 P.M.'];
 
 
-addEventListener("click",)
+
+var userInput = document.getElementById("userInput"); {
+    addEventListener("click", localStorage)
+    // localStorage.setItem();
+}
+
+
+
+var saveEvents = document.querySelector("userInput")
+
+function saveEvents() {
+    localStorage.addEventListener("click", saveButton)
+}
+
+
+window.onbeforeunload = function() {
+    localStorage.setItem(userInput ("userInput").val());
+}
+
+
+
+document.getElementById("userInput").innerHTML = localStorage.getItem("userInput")
+
+$('#saveButton').empty();
+
+    
